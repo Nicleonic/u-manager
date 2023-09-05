aba1902d8e5fb3bed3ea994421404ef57fbaaaf8
nicleonic <nicleonic2.0@gmail.com>
2023-09-05 09:52:25 +0200

    Responsive design of three main pages

--- script.js
+++ script.js
@@ -1,41 +1,77 @@
 "use strict";
 
+window.onload = function() {
+  // Code des interactions GUI de nicleonic
+  let cancellogin = document.getElementById('cancel');
+  cancellogin.addEventListener('click', (e) => {
+    toggle();
+    e.stopPropagation();
+  });
 
-window.onload = function(){
+  // Bouton de connexion
+  let login = document.getElementById("loginBtn");
+  login.addEventListener("click", (e) => {
+    toggle();
+    console.log('Clicked!');
+  });
+
+  // Page d'inscription
+  let signup = document.getElementById("signup");
+  signup.addEventListener("click", (e) => {
+    console.log("signup clicked");
+  });
+
+  let signBtn = document.getElementById("signBtn");
+  signBtn.addEventListener("click", () => {
+    console.log("Sign Btn was clicked!");
+  });
+};
+
+// Fonction de basculement
+function toggle() {
+  var blur = document.getElementById('loginpop');
+  blur.classList.toggle('active');
+}
 
 
-    // nicleonic code GUI interactions
-    let cancellogin = document.getElementById('cancel');
+// "use strict";
 
-    cancellogin.addEventListener('click', (e)=>{
-        toggle();
-        e.stopPropagation();
-    })
 
-    // --------------------- login Btn
-    let login = document.getElementById("loginBtn");
-    login.addEventListener("click",(e)=>{
-        toggle(); 
-        console.log('Clicked!');
-    })
+// window.onload = function(){
 
-    //    ------------------ signup page
-    let signup = document.getElementById("signup");
-    signup.addEventListener("click", (e)=>{
-        console.log("signup clicked");
-    })
 
-    let signBtn = document.getElementById("signBn");
-    signBtn.addEventListener("click", ()=>{
-        console.log("Sign Btn as clicked!");
-    })
+//     // nicleonic code GUI interactions
+//     let cancellogin = document.getElementById('cancel');
+
+//     cancellogin.addEventListener('click', (e)=>{
+//         toggle();
+//         e.stopPropagation();
+//     })
+
+//     // --------------------- login Btn
+//     let login = document.getElementById("loginBtn");
+//     login.addEventListener("click",(e)=>{
+//         toggle(); 
+//         console.log('Clicked!');
+//     })
+
+//     //    ------------------ signup page
+//     let signup = document.getElementById("signup");
+//     signup.addEventListener("click", (e)=>{
+//         console.log("signup clicked");
+//     })
+
+//     let signBtn = document.getElementById("signBn");
+//     signBtn.addEventListener("click", ()=>{
+//         console.log("Sign Btn as clicked!");
+//     })
    
-}
+// }
 
-// ----------------------- first function
-function toggle(){
-    var blur = document.getElementById('loginpop');
-    blur.classList.toggle('active')
-    // var connect = document.getElementById('connect-btn');
-    // popup.classList.toggle('active')
-}
+// // ----------------------- first function
+// function toggle(){
+//     var blur = document.getElementById('loginpop');
+//     blur.classList.toggle('active')
+//     // var connect = document.getElementById('connect-btn');
+//     // popup.classList.toggle('active')
+// }
