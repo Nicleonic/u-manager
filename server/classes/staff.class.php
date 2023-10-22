41d1a731894e2a91c63865a38f40a4e5044f5d52
nicleonic <nicleonic2.0@gmail.com>
2023-10-22 10:39:38 +0200

    admin--change

--- server/classes/staff.class.php
+++ server/classes/staff.class.php
@@ -1,29 +1,29 @@
 <?php
 
-                class staff extends tables{        
-                    public function __construct(){
-                        $bdd=new tables();
-                        $this->table="staff";
-                    }
-                }
-                $staff=new staff();
-                if (isset($_GET[$staff->table."-all"])) {
-                    $output=$staff->All();
-                }
-                if (isset($_GET[$staff->table."-new"])) {
-                    $output=$staff->new($_POST);
-                }
-                if (isset($_GET[$staff->table."-byId"])) {
-                    $output=$staff->byId($_GET[$staff->table."-byId"]);
-                }
-                if (isset($_GET[$staff->table."-update"])) {
-                    $output=$staff->update($_GET[$staff->table."-update"],$_POST);
-                }
-                if (isset($_GET[$staff->table."-delete"])) {
-                    $output=$staff->delete($_GET[$staff->table."-delete"]);
-                }
-                if (isset($_GET[$staff->table."-search"])) {
-                    $output=$staff->search($_POST);
-                }
+class staff extends tables{        
+    public function __construct(){
+        $bdd=new tables();
+        $this->table="staff";
+    }
+}
+$staff=new staff();
+if (isset($_GET[$staff->table."-all"])) {
+    $output=$staff->All();
+}
+if (isset($_GET[$staff->table."-new"])) {
+    $output=$staff->new($_POST);
+}
+if (isset($_GET[$staff->table."-byId"])) {
+    $output=$staff->byId($_GET[$staff->table."-byId"]);
+}
+if (isset($_GET[$staff->table."-update"])) {
+    $output=$staff->update($_GET[$staff->table."-update"],$_POST);
+}
+if (isset($_GET[$staff->table."-delete"])) {
+    $output=$staff->delete($_GET[$staff->table."-delete"]);
+}
+if (isset($_GET[$staff->table."-search"])) {
+    $output=$staff->search($_POST);
+}
 
             
