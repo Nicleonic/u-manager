6097cd02279d919ba2c8f92884a58cb7b3615baa
Frank M <64107540+frank-2001@users.noreply.github.com>
2023-10-22 02:10:56 +0200

    Speed php project 1.0

--- server/classes/etudiant_0.class.php
+++ server/classes/etudiant_0.class.php
@@ -1,32 +1,41 @@
 <?php
 
-class etudiant_0 extends tables{        
-    public function __construct(){
-        $bdd=new tables();
-        $this->table="etudiant_0";
-    }
-}
-$etudiant_0=new etudiant_0();
-if (isset($_GET[$etudiant_0->table."-all"])) {
-    $output=$etudiant_0->All();
-}
-if (isset($_GET[$etudiant_0->table."-new"])) {
-    // print_r($_POST);
-    $output=$etudiant_0->new($_POST);
-}
-if (isset($_GET[$etudiant_0->table."-byId"])) {
-    $output=$etudiant_0->byId($_GET[$etudiant_0->table."-byId"]);
-}
-if (isset($_GET[$etudiant_0->table."-update"])) {
-    $output=$etudiant_0->update($_GET[$etudiant_0->table."-update"],$_POST);
-}
-if (isset($_GET[$etudiant_0->table."-delete"])) {
-    $output=$etudiant_0->delete($_GET[$etudiant_0->table."-delete"]);
-}
-if (isset($_GET[$etudiant_0->table."-search"])) {
-    $output=$etudiant_0->search($_POST);
-}
-// $output=$etudiant_0->search(["nom"=>"shek"]);
+                class etudiant_0 extends tables{        
+                    public function __construct(){
+                        $bdd=new tables();
+                        $this->table="etudiant_0";
+                    }
+                }
+                
+                $etudiant_0=new etudiant_0();
+                if (isset($_GET[$etudiant_0->table."-all"])) {
 
+                    $output=$etudiant_0->All();
+                    // $temp=[];
+                    // $faculte=new faculte();
+                    // $faculte=$faculte->all()["data"];
+
+                    // foreach ($output["data"] as $key) {
+                    //     print_r($key);
+                    //     $key["faculte"]=$methods->get_element_by_key_value($faculte,"id",$key["faculte"]);
+                    //     $temp[count($temp)]=$key;
+                    // } 
+                    // $output["data"]=$temp;
+                }
+                if (isset($_GET[$etudiant_0->table."-new"])) {
+                    $output=$etudiant_0->new($_POST);
+                }
+                if (isset($_GET[$etudiant_0->table."-byId"])) {
+                    $output=$etudiant_0->byId($_GET[$etudiant_0->table."-byId"]);
+                }
+                if (isset($_GET[$etudiant_0->table."-update"])) {
+                    $output=$etudiant_0->update($_GET[$etudiant_0->table."-update"],$_POST);
+                }
+                if (isset($_GET[$etudiant_0->table."-delete"])) {
+                    $output=$etudiant_0->delete($_GET[$etudiant_0->table."-delete"]);
+                }
+                if (isset($_GET[$etudiant_0->table."-search"])) {
+                    $output=$etudiant_0->search($_POST);
+                }
 
             
--- server/index.php
+++ server/index.php
@@ -1,8 +1,8 @@
 <?php
-	
+
             header("Access-Control-Allow-Origin: *");
             header("Content-Type: application/json");
-            $output=array("message"=>"Aucune requete");
+            $output=array("message"=>"Aucune requete","state"=>true,"data"=>[]);
             require "classes.php";
             echo json_encode($output);
         
